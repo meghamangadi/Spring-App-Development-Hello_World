2726bb4f9d4d62ef95e868cd9aff0d0d7fb799eb
megha angadi <meghamangadi@gmail.com>
2021-11-06 11:31:46 +0530

    Added: a Helloworld Spring App to display “Hello from BridgeLabz” in REST Call

---
+++ src/main/java/com/bridgelabz/demo/controller/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello From BridgeLabz";
+	}
+
+}
