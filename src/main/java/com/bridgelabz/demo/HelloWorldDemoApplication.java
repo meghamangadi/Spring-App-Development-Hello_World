cb8567c55bb403d57448aeb8ef39d73b5b8a0bf2
megha angadi <meghamangadi@gmail.com>
2021-11-07 09:36:42 +0530

    Added: Logger to Log Messages using Built In SLF4J

--- src/main/java/com/bridgelabz/demo/HelloWorldDemoApplication.java
+++ src/main/java/com/bridgelabz/demo/HelloWorldDemoApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
 import com.bridgelabz.demo.component.DemoBean;
 import com.bridgelabz.demo.controller.HelloRestController;
 
@@ -15,9 +14,13 @@ public class HelloWorldDemoApplication {
 
 	public static void main(String[] args) {
 		logger.debug("Hello from BridgeLabz");
+		logger.debug("hello");
+		logger.info("demo");
+		logger.error("error");
+		logger.warn("warning");
 		ApplicationContext context = SpringApplication.run(HelloWorldDemoApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		logger.debug("Demo Bean = "+demoBean.toString());
+		logger.debug("Demo Bean = " + demoBean.toString());
 		System.out.println(context.getBean(HelloRestController.class));
 	}
 
--- src/main/java/com/bridgelabz/demo/component/DemoBean.java
+++ src/main/java/com/bridgelabz/demo/component/DemoBean.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DemoBean {
- public DemoBean() {}
+	public DemoBean() {
+	}
 
 }
